9eaf7681b4c583b94b52a128c4f75f660f47e2f2
Jestyn Apuya <jestynapuya@gmail.com>
2022-03-24 17:11:39 -0700

    Safety commit for demo, working on live data

--- client/components/Pie.jsx
+++ client/components/Pie.jsx
@@ -3,19 +3,20 @@ import {Pie} from 'react-chartjs-2';
 
 const data = {
   // labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  labels: ['Water', 'Exercise', 'Sleep', 'Study'],
+  labels: ['Incomplete',  'Complete'],
   // this is where we need to add the habits
   datasets: [
       {
       color:'#14797b',
       label: ['Habits'],
-      backgroundColor:[ '#c8d189','#1c5973', '#247f50', '#153131' ],
+      backgroundColor:[ '#c8d189', '#247f50' ],
       borderColor: '#14797b',
       borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      
+      hoverBackgroundColor: '#1c5973',
       hoverBorderColor: '#fafafa',
       // data: [65, 59, 80, 81, 56, 55, 40]
-      data: [60, 70, 50, 85]
+      data: [30,70]
       // this is where we need to add the data for the habits
     }
   ]
